9cb0a411c973afe103745f98ace7f164aaf82f86
BuildTools <unconfigured@null.spigotmc.org>
2023-08-28 14:32:23 +0800

    4

--- pages/link.vue
+++ pages/link.vue
@@ -10,7 +10,7 @@
         <table class="table table-bordered border-black table-success table-striped table-hover">
           <thead>
           <tr>
-            <th scope="col">#</th>
+
             <th scope="col">名稱</th>
             <th scope="col">連結</th>
 
@@ -19,24 +19,31 @@
           <tbody>
           <tr>
 
-            <th scope="row">1</th>
+
             <td>耶穌基督後期聖徒教會網站</td>
 
             <td> <NuxtLink class="nav-link" target="_blank" to="https://www.churchofjesuschrist.org/?lang=zho">連結</NuxtLink></td>
           </tr>
           <tr>
 
-            <th scope="row">1</th>
+
             <td>耶穌基督後期聖徒教會Youtube</td>
 
             <td> <NuxtLink class="nav-link" target="_blank" to="https://www.youtube.com/@MormonMessagesCHI">連結</NuxtLink></td>
           </tr>
           <tr>
 
-            <th scope="row">1</th>
+
             <td>台灣英文會話小組</td>
 
             <td> <NuxtLink class="nav-link" target="_blank" to="https://taiwaneng.com/">連結</NuxtLink></td>
+          </tr>
+          <tr>
+
+
+            <td>在線族譜及家庭樹資源• FamilySearch</td>
+
+            <td> <NuxtLink class="nav-link" target="_blank" to="https://www.familysearch.org/zh/home/">連結</NuxtLink></td>
           </tr>
           </tbody>
         </table>
